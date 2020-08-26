186823299d0e406e3c82be94dd6209acbd8675d8
arun-kmr-singh <parmaraks1@gmail.com>
2020-08-27 02:44:38 +0530

    Inital Commit

---
+++ Finder Reminder in Virual Environment/Assets/Scripts/PopUpText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpText : MonoBehaviour {
+
+    public Transform PlayerTransform;
+
+    float showOnDistance = 12;
+
+    private MeshRenderer textMesh;
+    // Use this for initialization
+    void Start () {
+        textMesh = gameObject.GetComponent<MeshRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Vector3.Distance(transform.position, PlayerTransform.position) < showOnDistance)
+            textMesh.enabled = true;
+        else
+            textMesh.enabled = false;
+    }
+}
